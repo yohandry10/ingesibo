d2da195bbdf1600d1216871ca04ceb54723c1d05
yohandry10 <149753134+yohandry10@users.noreply.github.com>
2024-11-07 10:59:42 -0500

    Delete ecommerce directory
    delete

--- ecommerce/backend/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ordersController = require('../controllers/ordersController');
-
-router.get('/', ordersController.getAllOrders);
-router.get('/:id', ordersController.getOrderById);
-
-module.exports = router;
